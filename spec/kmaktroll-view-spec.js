f4a3898398d7c7636f8ff27f85f502b7d0417b01
Roleke <roleke@Roleke-iMac.local>
2017-04-02 15:54:02 -0400

    d

---
+++ spec/kmaktroll-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KmaktrollView from '../lib/kmaktroll-view';
+
+describe('KmaktrollView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
